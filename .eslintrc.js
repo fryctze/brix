3269095bf13e33a85ac303ae3e8c799eb3859e6d
Fikry C. Farenza <vickyfarenza@gmail.com>
2023-06-15 14:25:14 +0700

    :rotating_light: Lint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": ['universe', 'plugin:react-hooks/recommended', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react", 'prettier'
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },  },
+    "rules": {
+    }
+}
