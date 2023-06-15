f12bad932250a038efdd31ddb0d9720a1d55f71c
Fikry C. Farenza <vickyfarenza@gmail.com>
2023-06-15 15:41:44 +0700

    :lipstick: Add Content component

---
+++ src/component/Content.tsx
@@ -0,0 +1,38 @@
+import { useTheme } from '@ui-kitten/components';
+import * as React from 'react';
+import { ScrollView, type ScrollViewProps } from 'react-native';
+
+interface ContentProps extends ScrollViewProps {
+  isPadding?: boolean;
+  level?: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | string;
+}
+
+const Content: React.FC<ContentProps> = ({
+  style,
+  contentContainerStyle,
+  children,
+  isPadding,
+  level,
+  ...props
+}) => {
+  const theme = useTheme();
+  return (
+    <ScrollView
+      {...props}
+      style={[
+        { backgroundColor: level && theme[`background-basic-color-${level}`] },
+        style,
+      ]}
+      showsHorizontalScrollIndicator={false}
+      scrollEventThrottle={16}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={[
+        contentContainerStyle,
+        isPadding && { paddingHorizontal: 24 },
+      ]}>
+      {children}
+    </ScrollView>
+  );
+};
+
+export default Content;
--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,6 +1,7 @@
 import Container from './Container';
+import Content from './Content';
 import HStack from './HStack';
 import Text from './Text';
 import VStack from './VStack';
 
-export { Text, Container, VStack, HStack };
+export { Text, Container, Content, VStack, HStack };
