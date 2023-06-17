861048d69545cc8c03f6cf0fb2e3842c9aa49099
Fikry C. Farenza <vickyfarenza@gmail.com>
2023-06-17 09:53:33 +0700

    :bento: Add image asset for intro screen

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+const Images = {
+  icon: require('./icon.jpg'),
+  icon_white: require('./icon-white.jpg'),
+  icon_small: require('./icon-small.jpg'),
+  student: require('./student.jpg'),
+  teacher: require('./teacher.jpg'),
+  intro1: require('./intro-1.jpg'),
+};
+export default Images;
