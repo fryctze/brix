6fe04bd13c8f663633b78c288e708ad46890c4df
Fikry C. Farenza <vickyfarenza@gmail.com>
2023-06-19 13:39:32 +0700

    :sparkles: design login

---
+++ src/assets/AssetsIconsPack.tsx
@@ -0,0 +1,40 @@
+import { IconPack, IconProvider } from '@ui-kitten/components';
+import React from 'react';
+import {
+  Image,
+  ImageProps,
+  ImageSourcePropType,
+  StyleSheet,
+} from 'react-native';
+import { SvgProps } from 'react-native-svg';
+
+import { Icons } from './icons';
+
+const createIcon = (source: ImageSourcePropType): IconProvider<ImageProps> => {
+  return {
+    toReactElement: props => (
+      <Image
+        style={styles.icon}
+        {...props}
+        source={source}
+        resizeMode="cover"
+      />
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
+
+const AssetsIconsPack: IconPack<ImageProps | SvgProps> = {
+  name: 'assets',
+  icons: {
+    eye: createIcon(Icons.eye),
+    eye_close: createIcon(Icons.eye_close),
+  },
+};
+export default AssetsIconsPack;
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export const Icons = {
+  eye: require('./ic_eye.png'),
+  eye_close: require('./ic_eye_close.png'),
+};
