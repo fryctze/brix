b811879658238783d58aa5bac13384e5481cc037
Fikry C. Farenza <vickyfarenza@gmail.com>
2023-06-21 13:05:16 +0700

    :bento: Add asset for bottom navigation

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,4 +3,10 @@ export const Icons = {
   eye_close: require('./ic_eye_close.png'),
   gg: require('./ic_google.png'),
   fb: require('./ic_facebook.png'),
+
+  home: require('./ic_home.png'),
+  class_icon: require('./ic_class.png'),
+  chat: require('./ic_chat.png'),
+  quiz: require('./ic_quiz.png'),
+  profile: require('./ic_profile.png'),
 };
